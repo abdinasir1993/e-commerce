a20b47875ac43488864c781600670e2296275b8a
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-07-28 17:48:01 +0100

    postman video updated

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ npm run watch
 ## video walk through
 
 [LINK](https://drive.google.com/file/d/1e2NLc6_AI8Pnz33Iy9RI5MswJQR81Q00/view)
-[LINK](https://drive.google.com/file/d/1-fEjUSM833HQ6HXNu9hEot0hBXkyyWj9/view)
+[LINK](https://drive.google.com/file/d/1ze3aeeaBYT00x4Z_zrdvBVEA7ci9gkO-/view)
 
 ## technology used
 
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -47,6 +47,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -68,7 +69,6 @@ router.post('/', (req, res) => {
       res.status(400).json(err);
     });
 });
-
 // update product
 router.put('/:id', (req, res) => {
   // update product data
@@ -111,8 +111,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const data = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!data) {
+      res.status(404).json({ message: 'product does not exist with this id' });
+      return;
+    }
+
+    res.status(200).json({ success: true });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
